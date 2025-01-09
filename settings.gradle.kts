6a6e5793033f51fb09b0601a658be9bea8bbab27
Viacheslav Kopeika <ks031088@gmail.com>
2025-01-09 19:14:31 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("app/libs/versions.toml"))
+        }
+    }
+
+}
+
+rootProject.name = "FishGallery"
+include(":app")
